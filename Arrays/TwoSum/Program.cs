f7ffef9ebc8291a791687ffb43a9b585f3c7948e
Dharshankuma <muthukumardharshan50@gmail.com>
2025-12-23 01:14:47 +0530

    Hashmap implementation for twosums

--- Arrays/TwoSum/Program.cs
+++ Arrays/TwoSum/Program.cs
@@ -1,10 +1,28 @@
 ﻿//LeetCode - TwoSum - Solution 
-// Approach : Brute Force 
-// Time Complexity : O(n^2)
-// Space Compmlexity : 0(1)
+
+
+
+
+public class Program
+{
+    static void Main()
+    {
+        int[] arr = { 2, 7, 11, 15 };
+        int target = 9;
+
+        TwoSumSolution solution = new TwoSumSolution();
+        int[] result = solution.TwoSum(arr, target);
+        Console.WriteLine($"Indices: [{result[0]},{result[1]}]");
+
+        int[] result2 = solution.TwoSumHashMap(arr, target);
+        Console.WriteLine($"Indices:[{result[0]},{result[1]}]");
+    }
+}
 
 public class TwoSumSolution
 {
+    // Approach : Brute Force 
+    // Time Complexity : O(n^2)
     public int[] TwoSum(int[] nums,int target)
     {
         int[] index = new int[2];
@@ -23,17 +41,27 @@ public class TwoSumSolution
 
         return index;
     }
-}
 
-public class Program
-{
-    static void main()
+    //Approach : Optimized (HashMap)
+    //Time Complexity : O(1)
+    public int[] TwoSumHashMap(int[] nums , int target)
     {
-        int[] arr = { 2, 7, 11, 15 };
-        int target = 9;
+        int n = nums.Length;
+        Dictionary<int, int> map = new Dictionary<int, int>();
+        for(int i = 0; i < n; i++)
+        {
+            int diff = target - nums[i];
+            if (map.ContainsKey(diff))
+            {
+                return new int[] { map[diff], i };
+            }
+            else
+            {
+                map[nums[i]] = i;
+            }
+        }
 
-        TwoSumSolution solution = new TwoSumSolution();
-        int[] result = solution.TwoSum(arr, target);
-        Console.WriteLine($"Indices: {result[0]}, {result[1]}");
+        return new int[] { };
     }
 }
+
