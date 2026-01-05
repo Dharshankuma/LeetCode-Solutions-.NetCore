4608d96681bc209ee3fee13bb6bcbc2c70af4cfa
Dharshankuma <muthukumardharshan50@gmail.com>
2026-01-05 21:45:48 +0530

    Plus One

---
+++ Arrays/PlusOne/Program.cs
@@ -0,0 +1,30 @@
+﻿class Solution
+{
+    public int[] PlusOne(int[] digits)
+    {
+        int n = digits.Length;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            if (digits[i] < 9)
+            {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] newNumber = new int[n + 1];
+        newNumber[0] = 1;
+        return newNumber;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+        int[] digits = { 1, 2, 3 };
+        int[] result = solution.PlusOne(digits);
+        System.Console.WriteLine(string.Join(",", result));
+    }
+}
