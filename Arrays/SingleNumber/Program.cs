1d9e4cd57345cb61ef88a8406f0c2f6ac65b27df
Dharshankuma <muthukumardharshan50@gmail.com>
2026-01-10 20:45:50 +0530

    Single Number

---
+++ Arrays/SingleNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿class SingleNumberSolution
+{
+    public int SingleNumber(int[] nums)
+    {
+        int result = 0;
+        foreach(int number in nums)
+        {
+            result ^= number;
+        }
+
+        return result;
+    }
+}
+
+
+class Program
+{
+    public static void Main()
+    {
+        SingleNumberSolution solution = new SingleNumberSolution();
+        int[] nums = {4, 1, 2, 1, 2};
+        int singleNumber = solution.SingleNumber(nums);
+        System.Console.WriteLine($"The single number in the array is: {singleNumber}");
+    }
+}
