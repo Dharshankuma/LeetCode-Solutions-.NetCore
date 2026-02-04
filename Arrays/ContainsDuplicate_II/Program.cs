82b3098b529df5e8388e8e522b0678131ae08529
Dharshankuma <muthukumardharshan50@gmail.com>
2026-02-04 09:51:58 +0530

    commit changes

---
+++ Arrays/ContainsDuplicate_II/Program.cs
@@ -0,0 +1,37 @@
+﻿class ContainseDuplicate
+{
+    public bool ContainsNearbyDuplicate(int[] nums,int k)
+    {
+        var numset = new HashSet<int>();
+        for(int i = 0; i < nums.Length; i++)
+        {
+            if (numset.Contains(nums[i]))
+            {
+                return true;
+            }
+            numset.Add(nums[i]);
+
+            if(numset.Count > k)
+            {
+                numset.Remove(nums[i - k]);
+            }
+
+        }
+
+        return false;
+    }
+}
+
+
+
+public class Program
+{
+    public static void Main()
+    {
+        ContainseDuplicate duplicate = new ContainseDuplicate();
+        int[] nums = { 1, 2, 3, 4, 1 };
+        int k = 3;
+        bool result = duplicate.ContainsNearbyDuplicate(nums, k);
+        Console.WriteLine(result);
+    }
+}
